f42f5a11c10efd877583d52b315dacc3498fbed2
Malek Chehab <34037921+MalekChehab@users.noreply.github.com>
2021-02-26 05:32:32 +0200

    added settings

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -4,4 +4,4 @@ final String SPLASH_SCREEN ='splashscreen';
 final String RECOVER = 'recover';
 final String NGO_SIGN_UP = 'ngosignup';
 final String HOME_PAGE = 'homepage';
-final String MAIN_PAGE = 'mainpage';
+final String SETTINGS = 'settings';
